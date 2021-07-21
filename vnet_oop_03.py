4e190b99427b3775b78c052613074aaef46c958c
Douglas Bezerra Beniz <douglasbeniz@yahoo.com.br>
2021-07-21 18:19:40 +0200

    Some improvements...

--- vnet_oop_03.py
+++ vnet_oop_03.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
+import os
+from time import sleep
+
 class ReceitaDeBolo:
     """ Classe base para receitas de bolo """
-    def __init__(self, qtdeFarinha:float, qtdeLeite:float, qtdeOvos:int, pesoUnidOvo:float=50, tBaterOvos:int=30, tBaterMassa:int=30) -> None:
+    def __init__(self, qtdeFarinha:float, qtdeLeite:float, qtdeOvos:int, pesoUnidOvo:float=50, tBaterOvos:int=30, tBaterMassa:int=30, tAssarBolo:int=30) -> None:
         """ Construtor da classe base """
         self.__qtdeFarinha:float    = qtdeFarinha   # atributo privado; quantidade de farinha; gramas
         self.__qtdeLeite:float      = qtdeLeite     # atributo privado; quantidade de leite; litros
@@ -10,6 +13,7 @@ class ReceitaDeBolo:
         self.__pesoUnidOvo:float    = pesoUnidOvo   # atributo privado; peso unitario dos ovos
         self.__tBaterOvos:int       = tBaterOvos    # atributo privado; tempo para bater os ovos; segundos
         self.__tBaterMassa:int      = tBaterMassa   # atributo privado; tempo para bater a massa; segundos
+        self.__tAssarBolo:int       = tAssarBolo    # atributo privado; tempo para assar o bolo; segundos
 
     """ Metodos publicos Get/Set do atributo quantidade de farinha (g) da receita """
     def getQtdeFarinha(self) -> float:
@@ -39,34 +43,79 @@ class ReceitaDeBolo:
     def setPesoUnidOvo(self, pesoUnidOvo:float) -> None:
         self.__pesoUnidOvo = pesoUnidOvo
 
-    """ Metodos publicos Get/Set do atributo tempo para bater os ovos """
+    """ Metodos publicos Get/Set do atributo tempo (s) para bater os ovos """
     def getTBaterOvos(self) -> int:
         return self.__tBaterOvos
 
     def setTBaterOvos(self, tBaterOvos:int) -> None:
         self.__tBaterOvos = tBaterOvos
 
-    """ Metodos publicos Get/Set do atributo tempo para bater a massa """
-    def getTBaserMassa(self) -> int:
+    """ Metodos publicos Get/Set do atributo tempo (s) para bater a massa """
+    def getTBaterMassa(self) -> int:
         return self.__tBaterMassa
 
     def setTBaterMassa(self, tBaterMassa:int) -> None:
         self.__tBaterMassa = tBaterMassa
 
-    """ Metodo privado que processa os ovos no preparo da massa """
+    """ Metodos publicos Get/Set do atributo tempo (s) para assar o bolo """
+    def getTAssarBolo(self) -> int:
+        return self.__tAssarBolo
+
+    def setTAssarBolo(self, tAssarBolo:int) -> None:
+        self.__tBaterMassa = tAssarBolo
+
+    """ Metodo privado que processa os ovos """
     def __baterOvos(self) -> None:
         print("Iniciando o processo de bater os ovos...")
         tempo:int
         for tempo in range(self.getTBaterOvos(), 0, -1):
-            print(f"Faltam {tempo} seg. para bater os ovos...", end="\r")
-        print("Ovos prontos para preparar o bolo!")
+            print(f"Faltam {tempo} seg. para terminar de bater os ovos...", end="\r")
+            sleep(1)    # considerando que o tempo informado esta em segundos
+        print("\nOvos prontos para preparar o bolo!")
 
+    """ Metodo privado que processa a mistura da massa """
+    def __baterMassa(self) -> None:
+        print("Iniciando o processo de misturar a massa do bolo...")
+        tempo:int
+        for tempo in range(self.getTBaterMassa(), 0, -1):
+            print(f"Faltam {tempo} seg. para terminar de misturar a massa...", end="\r")
+            sleep(1)    # considerando que o tempo informado esta em segundos
+        print("\nA massa esta pronta!")
+
+    """ Metodo privado que processa o cozimento do bolo """
+    def __assarMassa(self) -> None:
+        print("Iniciando o processo de cozimento do bolo...")
+        tempo:int
+        for tempo in range(self.getTAssarBolo(), 0, -1):
+            print(f"Faltam {tempo} seg. para terminar de assar o bolo...", end="\r")
+            sleep(1)    # considerando que o tempo informado esta em segundos
+        print("\nO bolo esta pronto!")
+
+    """ Metodo publico que prepara o bolo """
+    def prepararBolo(self) -> None:
+        print("# Preparando o bolo!")
+        print("#" + "-" * 79)
+        # Primeira etapa: bater os ovos
+        print("# Primeira etapa: os ovos!")
+        print("#" + "-" * 79)
+        self.__baterOvos()
+        # Segunda etapa: misturar a massa
+        print("#" + "-" * 79)
+        print("# Terceira etapa: a massa!")
+        print("#" + "-" * 79)
+        self.__baterMassa()
+        # Terceira etapa: assar o bolo
+        print("#" + "-" * 79)
+        print("# Quarta etapa: o bolo!")
+        print("#" + "-" * 79)
+        self.__assarMassa()
+        print("\n")
 
 class ReceitaDeBoloDeChocolate(ReceitaDeBolo):
     """ Classe extendida para receitas de bolo de chocolate """
-    def __init__(self, qtdeFarinha:float, qtdeLeite:float, qtdeOvos:int, qtdeChocolate:float) -> None:
+    def __init__(self, qtdeFarinha:float, qtdeLeite:float, qtdeOvos:int, qtdeChocolate:float, pesoUnidOvo:float=50, tBaterOvos:int=30, tBaterMassa:int=30, tAssarBolo:int=30) -> None:
         """ Construtor da classe extendida """
-        super().__init__(qtdeFarinha, qtdeLeite, qtdeOvos)
+        super().__init__(qtdeFarinha, qtdeLeite, qtdeOvos, pesoUnidOvo, tBaterOvos, tBaterMassa, tAssarBolo)
         self.__qtdeChocolate:float = qtdeChocolate   # atributo privado
 
     """ Metodos publicos Get/Set do atributo qtdeChocolate """
@@ -75,3 +124,28 @@ class ReceitaDeBoloDeChocolate(ReceitaDeBolo):
 
     def setQtdeChocolate(self, qtdeFarinha:float) -> None:
         self.__qtdeFarinha = qtdeFarinha
+
+
+
+# -----------------------------------------------------------------------------
+# Script de tests....
+# -----------------------------------------------------------------------------
+def main():
+    """ Funcao principal do script """
+
+    # Limpando a tela
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    # Iniciando...
+    print("#" + "-" * 79)
+    print("# RECEITA DE BOLOS ")
+    print("#" + "-" * 79)
+
+    # Instanciando o primeiro objeto de bolo de chocolate
+    boloParaLancheDaTarde = ReceitaDeBoloDeChocolate(200.0, 1.0, 3, 100.0, tBaterOvos=10)
+    #print(f"{boloParaLancheDaTarde.getQtdeFarinha():.2f}")
+    # Preparando o bolo...
+    boloParaLancheDaTarde.prepararBolo()
+
+if __name__ == "__main__":
+    main()
